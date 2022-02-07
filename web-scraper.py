6494f8d1e589a5e0b1e595894dbc51bd1f2ba63f
keabraekman <kea.braekman@mail.com>
2022-02-06 22:27:25 -0500

    divided pkl files

--- web-scraper.py
+++ web-scraper.py
@@ -34,7 +34,7 @@ def collectSoups(words, pages):
         pickle.dump(pickle_array, open('soups/names/'+words[i]+'.pkl', 'wb'))
 
 
-collectSoups(search_words, pages)
+# collectSoups(search_words, pages)
 
 
 # pickle > Array > soups > table > link > url > soup 
@@ -42,12 +42,73 @@ collectSoups(search_words, pages)
 # I need to find the links and make an array out of it
 
 
-def soup_to_link(soup):
-    # all_words = pickle.load(open('soups/names/Chin.pkl', 'rb'))
+
+# http://vm154.lib.berkeley.edu:3001/searchcase/fullDisplay?data=261039
+
+
+def soup_to_links(soup):
+    all_as = soup.find_all('a')
+    urls = []
+    for i in range(len(all_as)):
+        if 'fullDisplay' in all_as[i]['href']:
+            number = all_as[i]['href'].replace('/searchcase/fullDisplay?data=','')
+            url = 'http://vm154.lib.berkeley.edu:3001/searchcase/fullDisplay?data=' + number
+            urls.append(url)
+    return urls
+
+
+
+
     # print(all_words)
-    return len(soup)
+    # return len(soup)
 
 
-# soup = pickle.load(open('soups/names/Chin.pkl', 'rb'))
-# print(soup)
-# print(soup_to_link(soup[1]))
+
+
+
+# Now we need to go through each pickle file > Each array element > 
+# We want to have one pkl per name. The array is made up of the list of links
+
+
+soup = (pickle.load(open('soups/names/Chin.pkl', 'rb')))[1]
+# print(len(soup))
+
+# print(soup_to_links(soup))
+
+
+
+def create_individual_pages(words, pages):
+    for i in range(len(words)):
+        file = pickle.load(open('soups/names/' + words[i] + '.pkl', 'rb'))
+        urls = []
+        for j in range(pages[i]):
+            soup = file[j]
+            urls += soup_to_links(soup)
+        
+        pickle_array = []
+        for j in range(len(urls)):
+            url = urls[j]
+            print('requesting : ', url)
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            pickle_array.append(soup)
+        print('saving ' + 'soups/individual_pages/'+words[i]+'.pkl')
+        pickle.dump(pickle_array, open('soups/individual_pages/'+words[i]+'.pkl', 'wb'))
+
+
+# create_individual_pages(search_words, pages)
+
+
+
+# We need to divide Chin and Fong since they're above 100mbs
+
+def divide_pickle_array(pickle_array, fileName):
+    pickle.dump(pickle_array[0:int(len(pickle_array)/2)], open(fileName + '1.pkl', 'wb'))
+    pickle.dump(pickle_array[int(len(pickle_array)/2):len(pickle_array)], open(fileName + '2.pkl', 'wb'))
+
+
+chin = pickle.load(open('soups/individual_pages/Chin.pkl', 'rb'))
+divide_pickle_array(chin, 'soups/individual_pages/Chin')
+
+fong = pickle.load(open('soups/individual_pages/Fong.pkl', 'rb'))
+divide_pickle_array(fong, 'soups/individual_pages/Fong')
